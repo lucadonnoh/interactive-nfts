ec9aacb8659fb92fe343fe640121b98191598801
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2020-10-11 09:42:54 +0200

    fix: undefined sketch 'basic'->'line'

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   import Sketch from "./Sketch.svelte"
-  import {basic, clock, whiteBox} from "./sketch.js"
+  import {line, clock, whiteBox} from "./sketch.js"
 </script>
 
 <Sketch id="sketch1" sketch={whiteBox} />
