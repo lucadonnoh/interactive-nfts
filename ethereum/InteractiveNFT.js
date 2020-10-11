be4d268aa0ed7c48117c1320b50d29f2cfb3292c
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2020-10-11 17:54:24 +0200

    web3 setup

---
+++ ethereum/InteractiveNFT.js
@@ -0,0 +1,9 @@
+import web3 from './web3';
+import InteractiveNFT from './build/contracts/InteractiveNFT.json';
+
+const instance = web3.eth.Contract(
+    InteractiveNFT.abi,
+    '0x782A050fAA63b4Dbb83Dc9D37a1d84DB7be5A237'
+);
+
+export default instance;
--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,6 +1,8 @@
 <script>
   import Sketch from "./Sketch.svelte"
   import {line, clock, whiteBox} from "./sketch.js"
+  import web3 from "../../ethereum/web3";
+  import InteractiveNFT from '../../ethereum/InteractiveNFT';
 </script>
 
 <Sketch id="sketch1" sketch={whiteBox} />
