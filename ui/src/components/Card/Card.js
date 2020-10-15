1e6e01fb574f8707dce7713ec5bcdf992d3766e7
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2020-10-15 19:07:38 +0200

    fixed token rendering

--- ui/src/components/Card/Card.js
+++ ui/src/components/Card/Card.js
@@ -4,20 +4,13 @@ import "./Card.css";
 class Card extends React.Component {
     constructor(props) {
         super(props);
-        console.log(this.props);
-
-        this.state = {
-            title: props.title,
-            description: props.description,
-            image: props.image
-        }
     }
 
     render() {
         return(
             <div className="card">
-                <h3>{this.state.title}</h3>
-                <h4>{this.state.description}</h4>
+                <h3>{this.props.title}</h3>
+                <h4>{this.props.description}</h4>
             </div>
         )
     }
--- ui/src/pages/Collection/Collection.js
+++ ui/src/pages/Collection/Collection.js
@@ -3,9 +3,11 @@ import React from 'react';
 import './Collection.css';
 import Layout from '../../components/Layout';
 import Card from '../../components/Card/Card';
+import { Button } from 'semantic-ui-react';
 
 import initWeb3 from "services/web3";
 import initContract from "services/InteractiveNFT";
+const fetch = require('node-fetch');
 
 class Collection extends React.Component {
 
@@ -13,36 +15,19 @@ class Collection extends React.Component {
     super(props);
 
     this.state = {
-      title: "",
-      description: "",
+      title: "pop",
+      description: "pup",
       image: ""
     }
 
-    this.getJSON = this.getJSON.bind(this);
   }
 
-  getJSON(url, callback) {
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', url, true);
-      xhr.responseType = 'json';
-      xhr.onload = () => {
-        var status = xhr.status;
-        if (status === 200) {
-          callback(null, xhr.response);
-          this.setState({title: xhr.response.properties.name.description});
-          console.log(this.state.title); //works
-        } else {
-          callback(status, xhr.response);
-        }
-      };
-      xhr.send();
-    };
-
   componentDidMount = async () => {
-
+    var that = this;
     let web3 = await initWeb3();
     let InteractiveNFT = initContract(web3);
 
+    console.log(web3, InteractiveNFT);
     let accounts = await web3.eth.getAccounts();
 
     this.setState({
@@ -56,15 +41,22 @@ class Collection extends React.Component {
       .methods
       .tokenURI(10).call();
 
-    this.getJSON(URI,
-      function (err, data) {
-        if (err !== null) {
-          alert('Something went wrong: ' + err);
-        } else {
-          console.log(data.properties.name.description); //works
-        }
+    console.log(URI);
+
+    let settings = { method: "Get" };
+
+    let t = '';
+
+    await fetch(URI, settings)
+      .then(res => res.json())
+      .then((json) => {
+        console.log("that:", that.state);
+        that.setState({
+          title: json.properties.name.description,
+          description: json.properties.description.description,
+          image: json.properties.image.description
+        });
       });
-    console.log(this.state.title); //doesnt work: <empty string>
   }
 
   render() {
